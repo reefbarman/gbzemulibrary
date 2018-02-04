e90949ac16ad75173d8a1091cb86f883bd38e3ee
Tristan Cartledge <tristan.cartledge@gmail.com>
2018-02-04 15:37:15 +0000

    cleanup

--- GBZEmuLibrary/Emulator.cs
+++ GBZEmuLibrary/Emulator.cs
@@ -103,9 +103,9 @@ namespace GBZEmuLibrary
                 _cpu.UpdateInterrupts();
 
                 _clocksThisFrame += _clocksThisUpdate;
-            } while (_clocksThisFrame < (CLOCKS_PER_CYCLE * _cpu.SpeedFactor));
+            } while (_clocksThisFrame < CLOCKS_PER_CYCLE);
 
-            _clocksThisFrame -= (CLOCKS_PER_CYCLE * _cpu.SpeedFactor);
+            _clocksThisFrame -= CLOCKS_PER_CYCLE;
         }
 
         public Color[,] GetScreenData()
@@ -135,9 +135,9 @@ namespace GBZEmuLibrary
 
         private void UpdateSystems(int cycles)
         {
-            _clocksThisUpdate += cycles;
-
             cycles /= _cpu.SpeedFactor;
+
+            _clocksThisUpdate += cycles;
 
             _divideRegister.Update(cycles);
             _timer.Update(cycles);
