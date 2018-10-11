18392041219c581d745a9b5064f7e09e83c9ad6c
Tristan Cartledge <tristan.cartledge@gmail.com>
2018-10-11 19:43:57 +0100

    add BootRom.cs shim

---
+++ GBZEmuLibrary/Core/BootROM.cs
@@ -0,0 +1,37 @@
+﻿namespace GBZEmuLibrary
+{
+    internal static class BootROM
+    {
+        public static void SetBootMode(bool gbc, bool quickAnim)
+        {
+            if (gbc)
+            {
+                Bytes = GBCBootROM;
+            }
+            else
+            {
+                Bytes = Raw;
+
+                if (quickAnim)
+                {
+                    Bytes         = ShortRaw;
+                    Bytes[0x00FD] = 0x03;
+                }
+            }
+        }
+
+        public static byte[] Bytes { get; private set; }
+
+        private static byte[] Raw { get; } =
+        {
+        };
+
+        private static byte[] ShortRaw { get; } =
+        {
+        };
+
+        public static byte[] GBCBootROM { get; } =
+        {
+        };
+    }
+}
